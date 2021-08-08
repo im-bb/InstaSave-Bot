a882db60a20ac45c309bbd4fb734d3298997d2dd
im-bb <87819247+im-bb@users.noreply.github.com>
2021-08-08 07:45:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -192,12 +192,12 @@ async def upload(m, bot, chat_id, dir):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-					InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")
+					InlineKeyboardButton("Developer", url='https://t.me/im-bb1'),
+					InlineKeyboardButton("Other Useful Bots", url="https://t.me/jm_support")
 				],
 				[
-					InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
-                    InlineKeyboardButton("⚡️Update Channel", url="https://t.me/subin_works")
+					InlineKeyboardButton("Source Code", url="https://github.com/im-bb/InstaSave-Bot"),
+                    InlineKeyboardButton("⚡️Update Channel", url="https://t.me/jm_support")
 				]
 			]
 			)
